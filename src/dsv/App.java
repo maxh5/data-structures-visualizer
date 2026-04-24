dbbb2b70fd2332a5d58dcaec77e66945297855be
Max <maxh5@icloud.com>
2026-04-23 17:47:17 -0700

    Icon & Borderpane fixed

--- src/dsv/App.java
+++ src/dsv/App.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -13,6 +14,12 @@ public class App extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("main/main-view.fxml"));
         primaryStage.setTitle("Data Structures Visualizer");
+        
+        var iconResource = getClass().getResource("/dsv/resources/icon.png");
+        if (iconResource != null) {
+            primaryStage.getIcons().add(new Image(iconResource.toExternalForm()));
+        }
+        
         primaryStage.setScene(new Scene(root));
 
         primaryStage.show();
